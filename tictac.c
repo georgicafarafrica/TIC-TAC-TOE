0619d689bc2ce7c1f2d54098173ecc3202fa8495
georgicafarafrica <burlo.george@yahoo.com>
2025-05-21 00:22:34 +0300

    Add files via upload

--- tictac.c
+++ tictac.c
@@ -1,12 +1,17 @@
 #include <raylib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define BIGBOARD_SIZE 9
 #define SMALLBOARD_SIZE 3
+
 typedef struct {
   int value;
 }Cell;
-
+// RESETARE PATRAT - POWER-UP 0
+// INLOCUIRE - POWER-UP 1
+// SCHIMBARE DIRECTIE - POWER-UP 2
 typedef struct {
   Cell smallBoard[SMALLBOARD_SIZE][SMALLBOARD_SIZE];
   int winner;
@@ -80,12 +85,6 @@ int CheckActualWinner(){
   return -1;
 }
 
-void UpdateFields(int y, int z){
-      for (int i = 0; i < SMALLBOARD_SIZE; i ++)
-	for (int j = 0; j < SMALLBOARD_SIZE; j++)
-	  if (tct.squares[y][z].smallBoard[i][j].value == -2)
-	     tct.squares[y][z].smallBoard[i][j].value = -1;
-}
 void DrawTable(){
   ClearBackground(BLACK);
   for (int i = 1; i < SMALLBOARD_SIZE; i++){
@@ -144,10 +143,12 @@ void MouseControl(int x, int y){
        if (x < BIGBOARD_SIZE && y < BIGBOARD_SIZE && tct.squares[y/3][x/3].smallBoard[y%3][x%3].value == -1) {
 	 tct.squares[y/3][x/3].smallBoard[y%3][x%3].value = player;
     }
+    if((y/3) < 3 && (x/3) < 3){
       if (player == 1)
 	player = 0;
       else
 	player = 1;
+    }
 }
 int NoMoreCells(){
   for (int a = 0; a < SMALLBOARD_SIZE;a++)
@@ -168,9 +169,27 @@ int IsSquareFull(int x, int y){
 }
 
 int SquareAvailability(int x, int y){
-  if (tct.squares[y%3][x%3].values == -1)
+  if (tct.squares[y][x].values == -1)
     return 1;
   return 0;
+}
+
+void Powerup0(int x, int y){// se selecteaza un patrat si se sterge tot din acel patrat// daca acel patrat nu este deja castigat
+    for (int i = 0 ; i < SMALLBOARD_SIZE; i++)
+      for (int j = 0; j < SMALLBOARD_SIZE; j++)
+        tct.squares[y/3][x/3].smallBoard[i][j].value = -1;
+}
+
+void YPowerup1(int x, int y){
+  int *cell = &tct.squares[y/3][x/3].smallBoard[y%3][x%3].value;
+  if (*cell == 1)
+    *cell = 0;
+}
+
+void XPowerup1(int x, int y){
+  int *cell = &tct.squares[y/3][x/3].smallBoard[y%3][x%3].value;
+  if (*cell == 0)
+    *cell = 1;
 }
 
 void ResetGame(){
@@ -180,57 +199,109 @@ void ResetGame(){
 }
 
 int main(){
+  srand(time(NULL));
   int x = 0,y = 0;
   int memorizeX = 0, memorizeY = 0;
-  InitWindow(1100,900, "TIC-TAC-TOE"); 
-  SetWindowPosition(GetScreenWidth()-300,-GetScreenHeight());
+  int playerW = -1;
+  int powerUp = 0;
+  int rand_int = -1, randA = 0;
+  int PmemorizeX = 0, PmemorizeY = 0;
+  InitWindow(1400,900, "TIC-TAC-TOE"); 
+  SetWindowPosition(GetScreenWidth()-600,-GetScreenHeight());
   SetTargetFPS(60);
   InitialiseBigBoard();
   InitialiseSmallBoards();
   while(!WindowShouldClose()){
-    if (player == 1)
+    if (player == 1 && playerW == -1 && powerUp == 0)
       DrawText("X turn :",910,50,40,WHITE);
-    else
+    if (player == 0 && playerW == -1 && powerUp == 0)
       DrawText("0 turn :",910,50,40,WHITE);
+    if (powerUp == 1 && playerW == -1 && rand_int == 0)
+      DrawText("Clear a square : ",910,50,40,WHITE);
+    if (powerUp == 1 && playerW == -1 && rand_int == 1)
+      DrawText("Swap one opponent move: ",900,50,40,WHITE);
+    if (powerUp == 1 && playerW == -1 && rand_int == 2){
+      DrawText("Select a small square: ",910,50,40,WHITE);
+      DrawText("so your opponent can play: ",910,80,40,WHITE);
+    }
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
     	x = GetMouseX() ;
     	y = GetMouseY();
 	CheckOneBoardWinner();
-	if (!EmptyCells() && SquareAvailability(memorizeX,memorizeY) && !IsSquareFull(memorizeX,memorizeY)){
-	  if (x > (memorizeX%3) * 300 && x < ((memorizeX%3) * 300)+300 && y > (memorizeY%3)*300 && y < ((memorizeY % 3)*300 + 300)){// && (tct.squares[(y/100)%3][(x/100)%3].values == -1)){
+	if ( powerUp == 0 && !EmptyCells() && SquareAvailability(memorizeX%3,memorizeY%3) && !IsSquareFull(memorizeX,memorizeY)){
+	  if (x > (memorizeX%3) * 300 && x < ((memorizeX%3) * 300)+300 && y > (memorizeY%3)*300 && y < ((memorizeY % 3)*300 + 300)){
 	    MouseControl(x/100,y/100);
 	    memorizeX = x/100;
 	    memorizeY = y/100;
+      randA = 0;
 	  }
-    /*else if(!SquareAvailability(x/100,y/100)){
-      MouseControl(x/100,y/100);
-	    memorizeX = x/100;
-	    memorizeY = y/100;
-    }*/
 	}
-	else{
+	else if (powerUp == 0){
 	  MouseControl(x/100,y/100);
 	  memorizeX = x/100;
 	  memorizeY = y/100;
-	}
+	  }
+  }
+  CheckOneBoardWinner();
+  BeginDrawing();
+  if (!SquareAvailability(memorizeX/3,memorizeY/3)){
+    powerUp = 1;
+    if (!randA){
+      rand_int = rand() % 3;
+      randA = 1;
     }
-    CheckOneBoardWinner();
-    BeginDrawing();
-    if (!EmptyCells() && SquareAvailability(memorizeX,memorizeY) && !IsSquareFull(memorizeX,memorizeY)) //&& SquareAvailability(x/100,y/100))//  && (tct.squares[(y/100)%3][(x/100)%3].values == -1))
+    if (rand_int == 0){
+      if (x/100 < 9 && y/100 < 9 && SquareAvailability(x/300,y/300)){
+        Powerup0(x/100,y/100);
+        powerUp = 0;
+        //randA = 0;
+    }
+  }
+    else if (rand_int == 1){
+          if (x/100 < 9 && y/100 < 9 && SquareAvailability(x/300,y/300) && tct.squares[y/3][x/3].smallBoard[y%3][x%3].value != -1){
+            if (player == 0)
+              XPowerup1(x/100, y/100);
+            if (player == 1)
+              YPowerup1(x/100,y/100);
+            powerUp = 0;
+            //randA = 0;
+            }
+      }
+    else if (rand_int == 2){
+      if (x/100 < 9 && y/100 < 9 && SquareAvailability(x/300,y/300)){
+        DrawRectangle((x/300)*300,(y/300)*300,300,300,LIME);
+        memorizeX = x/300;
+        memorizeY = y/300;
+        powerUp = 0;
+        //randA = 0;
+      }
+    }
+  }
+    if (powerUp == 0 && playerW == -1 && !EmptyCells() && SquareAvailability(memorizeX%3,memorizeY%3) && !IsSquareFull(memorizeX,memorizeY)) //&& SquareAvailability(x/100,y/100))//  && (tct.squares[(y/100)%3][(x/100)%3].values == -1))
 	    DrawRectangle(((memorizeX)%3)*300,((memorizeY)%3)*300,300,300,LIME);
+    else{
+      if (playerW == -1 && powerUp == 0){
+        DrawText("You can play wherever", 910, 150,40,WHITE);
+        DrawText("you want", 910, 190, 40 , WHITE);
+      }
+    }
     DrawTable();
-    int playerW = CheckActualWinner();
+    DrawRectangle(900,800,500,100, GRAY);
+    DrawText("RESTART",915,800,100, YELLOW);
+    playerW = CheckActualWinner();
     if (playerW != -1){
       if (playerW == 1)
-	      DrawText("PLAYER X WON !!!", 350, 450, 60, GREEN);
+	      DrawText("PLAYER X WON !!!", 910, 190, 60, GREEN);
       if (playerW == 0)
-	      DrawText("PLAYER 0 WON !!!", 350, 450, 60, GREEN);
+	      DrawText("PLAYER 0 WON !!!", 910, 190, 60, GREEN);
       
     }
-    if (NoMoreCells())
-      DrawText("REMIZA !!!", 350, 450, 60, GREEN);
-    if (IsKeyPressed('R'))
-      ResetGame();
+  if (NoMoreCells())
+    DrawText("REMIZA !!!", 910, 190, 60, YELLOW);
+  if (x > 900 && y > 800){
+    ResetGame();
+    powerUp = 0;
+  }
   EndDrawing();
   }
   CloseWindow();
